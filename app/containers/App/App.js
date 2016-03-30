9e842f42caa43be7c86f2533484e127e87044e35
Mihir Soni <mihirsoni.123@gmail.com>
2016-03-30 10:29:41 +0530

    adding intial todo

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -1,14 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as todoActions from 'redux/reducers/todo';
 
-export default class App extends Component {
+class App extends Component {
+  static propTypes = {
+    todos: PropTypes.array.isRequired
+  }
   state = {
     show: false
   }
   render() {
+    const { todos } = this.props;
+    console.log('all todos are ', todos);
     return (
       <div>
-        Hello World Mihir !!!
+        {todos.map(todo =>
+          <div>
+            {todo.text}
+            </div>
+        )}
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  console.log('state for mihir is ', state);
+  return {
+    todos: state.todo
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(todoActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
